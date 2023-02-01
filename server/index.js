3187b18c4732eb779d90134d4f23d28b48281756
prabhat_021 <prabhatsahrawat010203@gmail.com>
2023-02-01 19:48:22 +0530

    monogodb setup

--- server/index.js
+++ server/index.js
@@ -2,11 +2,27 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
+dotenv.config();
+const PORT = process.env.PORT;
 const app = express();
+app.use(express.json());
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+mongoose.set('strictQuery', false);
+const connectDb = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDb Connected To ${connect.connection.host}`);
+    } catch (error) {
+        console.error(error);
+        process.exit();
+    }
+}
 
+connectDb();
+app.listen(PORT, console.log(`Server is listening at Port:-${PORT}`));
