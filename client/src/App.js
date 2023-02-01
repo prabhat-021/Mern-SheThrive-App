4e402b051ab12cb3d27737608fb4d8a381d4e770
prabhat_021 <prabhatsahrawat010203@gmail.com>
2023-02-02 01:53:04 +0530

    Post File added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
+import React from "react";
+import { Container, Grid, AppBar, Typography, Grow } from "@material-ui/core";
+import memories from "./images/memories.png";
+import Form from "./Components/Form/Form.js";
+import Posts from "./Components/Posts/Posts.js";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      MY App
-    </div>
+    <Container maxWidth="xl">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="centre">Memories</Typography>
+        <img src={memories} alt="memories" height="60" />
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="streach" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </AppBar>
+    </Container>
   );
 }
 
-export default App;
