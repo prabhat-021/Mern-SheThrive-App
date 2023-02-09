2bb8494afab6e17f19c984070221cab7011f353a
prabhat_021 <prabhatsahrawat010203@gmail.com>
2023-02-10 02:52:54 +0530

    Post section updated

--- client/src/Components/Posts/Post/Post.js
+++ client/src/Components/Posts/Post/Post.js
@@ -1,7 +1,31 @@
-export default function Post() {
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@material-ui/core";
+import useStyles from "./style.js";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUp";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
+
+
+export default function Post({ post }) {
+
+    const classes = useStyles();
+
     return (
-        <section>
-            Post
-        </section>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{ color: "white" }} size="small" onClick={() => { }}>
+                    <MoreHorizIcon fontSize="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tags) => `#${tags}`)}</Typography>
+            </div>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+        </Card>
     );
 }
