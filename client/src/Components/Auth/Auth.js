28610d07583948b00801dc085e036ede6004261b
prabhat_021 <prabhatsahrawat010203@gmail.com>
2023-04-05 23:51:24 +0530

    Login signup and google signin functionality

--- client/src/Components/Auth/Auth.js
+++ client/src/Components/Auth/Auth.js
@@ -1,14 +1,16 @@
-import { Avatar, Container, Grid, Paper, TextField, Typography } from "@material-ui/core";
+import { Avatar, Button, Container, Grid, Paper, Typography } from "@material-ui/core";
 import useStyles from "./styles.js";
 import LockOutlineIcon from "@material-ui/icons/LockOutlined";
 import Input from "./Input.js";
 import { useState } from "react";
+import { GoogleLogin } from "react-google-login";
+import Icon from "./Icon.js";
 
 export default function Auth() {
 
     const classes = useStyles();
-    const isSignup = !false;
-    const[showPassword,setShowPassword]=useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
 
     function handleSubmit() {
 
@@ -17,9 +19,21 @@ export default function Auth() {
     function handleChange() {
 
     }
-    
-    function handleShowPassword() {
 
+    function switchMode() {
+        setIsSignup(!isSignup);
+    }
+
+    function handleShowPassword() {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    }
+
+    function GoogleFailure() {
+        console.error("Google Sign In was unsuccessfull. Try Again Later");
+    }
+
+    function GoogleSuccess(res) {
+        console.log(res);
     }
 
     return (
@@ -34,37 +48,80 @@ export default function Auth() {
                         {
                             isSignup && (
                                 <>
-                                   
-                                        <Input
-                                            name="firstname"
-                                            label="First Name"
-                                            onChange={handleChange}
-                                            autoFocus
-                                            half
-                                        />
-                                        <Input
-                                            name="lastname"
-                                            label="Last Name"
-                                            onChange={handleChange}
-                                            autoFocus
-                                            half
-                                        />
+
+                                    <Input
+                                        name="firstname"
+                                        label="First Name"
+                                        onChange={handleChange}
+                                        autoFocus
+                                        half
+                                    />
+                                    <Input
+                                        name="lastname"
+                                        label="Last Name"
+                                        onChange={handleChange}
+                                        autoFocus
+                                        half
+                                    />
                                 </>
                             )
                         }
-                        <Input 
+                        <Input
                             name="email"
                             label="Email Address"
                             onChange={handleChange}
                             type="email"
                         />
-                        <Input 
+                        <Input
                             name="password"
                             label="Password"
                             onChange={handleChange}
                             type={showPassword ? "text" : "password"}
                             handleShowPassword={handleShowPassword}
                         />
+                        {
+                            isSignup && (
+                                <Input
+                                    name="confirmPassword"
+                                    label="Repeat Password"
+                                    onChange={handleChange}
+                                />
+                            )
+                        }
+                    </Grid>
+                    <GoogleLogin
+                        clientId="509852608937-uf3g3c7lhh8dn8oens52k6bku9a05jbl.apps.googleusercontent.com"
+                        render={(renderProps) => {
+                            <Button
+                                className={classes.googleButton}
+                                color="primary"
+                                fullWidth
+                                onClick={renderProps.disabled}
+                                startIcon={<Icon />}
+                                variant="contained"
+                            >
+                                Google Sign In
+                            </Button>
+                        }}
+                        onSuccess={GoogleSuccess}
+                        onFailure={GoogleFailure}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        {isSignup ? "Sign Up" : "Sign In"}
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {isSignup ? "Already have an Account? Sign In" : "Don't have an Account? Sign Up"}
+                            </Button>
+                        </Grid>
                     </Grid>
                 </form>
             </Paper>
