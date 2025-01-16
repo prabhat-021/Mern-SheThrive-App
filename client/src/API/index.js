bdbfc3a247842f4e9c9a87cac98a7732f667e074
Prabhat Sehrawat <prabhatsahrawat010203@gmail.com>
2025-01-16 19:27:19 +0530

    Deploy

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "/posts";
+const url = "https://mern-she-thrive-app.vercel.app/posts";
 
 axios.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
@@ -12,16 +12,16 @@ axios.interceptors.request.use((req) => {
 
 // POST 
 
-export const fetchPosts = (page) => axios.get(`/posts?page=${page}`);
-export const fetchPostsById = (id) => axios.get(`/posts/${id}/id`);
-export const fetchPostsBySearch = (searchQuery) => axios.get(`/posts/search?searchQuery=${searchQuery.search || "none" }&tags=${searchQuery.tags}`);
+export const fetchPosts = (page) => axios.get(`${url}?page=${page}`);
+export const fetchPostsById = (id) => axios.get(`${url}/${id}/id`);
+export const fetchPostsBySearch = (searchQuery) => axios.get(`${url}/search?searchQuery=${searchQuery.search || "none" }&tags=${searchQuery.tags}`);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
-export const comment = (value, id) => axios.post(`/posts/${id}/commentPost`, { value })
+export const comment = (value, id) => axios.post(`${url}/${id}/commentPost`, { value })
 
 //  USER 
 
-export const signIn = (formData) => axios.post('/user/signin', formData);
-export const signUp = (formData) => axios.post('/user/signun', formData);
+export const signIn = (formData) => axios.post(`${url}/user/signin`, formData);
+export const signUp = (formData) => axios.post(`${url}/user/signun`, formData);
