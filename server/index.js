d1783723da6ea4544c3c4bd34ea733389f5e0460
Prabhat Sehrawat <prabhatsahrawat010203@gmail.com>
2025-01-16 18:29:43 +0530

    deploying

--- server/index.js
+++ server/index.js
@@ -42,19 +42,22 @@ const connectDb = async () => {
     }
 }
 
-const __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/build")));
+// const __dirname = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "/client/build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    })
-} else {
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     })
+// } else {
     
-    app.get("/", (req, res) => {
-        res.send("API is running...");
-    });
-}
+//     app.get("/", (req, res) => {
+//         res.send("API is running...");
+//     });
+// }
 
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
 connectDb();
 app.listen(PORT, console.log(`Server is listening at Port:-${PORT}`));
